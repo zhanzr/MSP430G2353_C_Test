7be5b5eeecb35821f4713cc7d5baa7222cd675ce
ZHAN Zongru <zhanzr@foxmail.com>
2017-10-09 01:30:41 +0800

    main loop initial beta

--- main.c
+++ main.c
@@ -42,19 +42,278 @@ __attribute__((interrupt(COMPARATOR_A0))) void COM_A0_ISR()    {
 //		PUSH    R12
 //		PUSH    R15
 //		PUSH    R14
-	TA0CTL |= 0x04;
+	uint16_t* p_r4;
+	uint16_t r14;
+	uint16_t r15;
+
+	TA0CTL |= BIT2;
 	if(0 != (BIT0 & TA0CCTL0))
 	{
 		*(uint8_t*)0x021d = 0x11;
 		*(uint8_t*)0x021e = 0;
 	}
+	TA0CCTL0 &= ~BIT0;
+	TA0CCR0 = 0x010c;
+	TA0CCTL1 &= ~BIT0;
+	TA0CCR1 = 0x0044;
+	if(*(uint8_t*)0x021d >= 0x0a)
+	{
+		p_r4 = 0x0200;
+	}
+	else
+	{
+		if(*(uint8_t*)0x021d >= 8)
+		{
+			p_r4 = 0x0201;
+		}
+		else
+		{
+			p_r4 = 0x0202;
+		}
+	}
 
+	while(0==(TA0CCTL1 & BIT0))
+	{
+		;
+	}
+
+	MeasCAOut_fcb2();
+
+	*(uint8_t*)0x0206 = *(uint8_t*)0x0206 / 4;
+
+	*p_r4 = *p_r4 / 2;
+
+	*(uint8_t*)0x021d = *(uint8_t*)0x021d - 1;
+	if(0 != *(uint8_t*)0x021d)
+	{
+		goto COM_A_ISR_EXIT_F5E4;
+	}
+
+	*(uint8_t*)0x0201 = *(uint8_t*)0x0201 & 0xc0;
+	if(0 == *(uint8_t*)0x021e)
+	{
+		TA0CCR0 = 0x01c8;
+
+	}
+	else
+	{
+		goto l_f180;
+	}
+
+	while(0==(TA0CCTL0 & BIT0))
+	{
+		;
+	}
+
+	TA0CTL |= BIT2;
+	TA0CCR0 = 0x0878;
+	TA0CCTL0 &= ~BIT0;
+	*(uint16_t*)0x0212 = *(uint16_t*)0x0200;
+	//CLRC
+	*(uint8_t*)0x0202 = *(uint8_t*)0x0202 / 2;
+
+	r14 = *(uint8_t*)0x0202;
+	*(uint8_t*)0x021c = r14;
+	r15 = *(uint8_t*)0x0200;
+	r15 += *(uint8_t*)0x0201;
+	r15 += r14;
+	*(uint8_t*)0x0220 = r15;
+	if((r14 == 0x3c) || (r14 == 0x4f))
+	{
+		*(uint16_t*)0x0210 |= BIT0;
+		*(uint8_t*)0x021d = 8;
+	}
+	else
+	{
+		*(uint16_t*)0x0210 &= ~BIT0;
+		*(uint8_t*)0x021d = 0x11;
+	}
+
+	*(uint8_t*)0x0240 = f_fa06(*(uint16_t*)0x0212);
+	if(*(uint8_t*)0x0240 == *(uint8_t*)0x0203)
+	{
+		*(uint8_t*)0x021a &= ~BIT6;
+		*(uint16_t*)0x0210 |= BIT1;
+		if(BIT1 & *(uint8_t*)0x021a)
+		{
+			goto l_f170;
+		}
+		else
+		{
+			goto l_f174;
+		}
+	}
+	else
+	{
+		r14 = *(uint8_t*)0x0240;
+		r15 = *(uint8_t*)0x0203;
+		r15 ++;
+		if(r15 == r14)
+		{
+			*(uint8_t*)0x021a |= BIT6;
+			*(uint16_t*)0x0210 |= BIT1;
+			if(BIT4 & *(uint8_t*)0x021a)
+			{
+				goto l_f170;
+			}
+			else
+			{
+				goto l_f174;
+			}
+		}
+		else
+		{
+			goto l_f170;
+		}
+	}
+l_f170:
+	*(uint16_t*)0x0210 &= ~BIT1;
+l_f174:
+	*(uint8_t*)0x021e = 1;
+	*(uint8_t*)0x021f = 2;
+	goto COM_A_ISR_EXIT_F5E4;
+
+l_f180:
+
+COM_A_ISR_EXIT_TA0IFG_F5E0:
+	TA0CCTL0 |= BIT0;
+COM_A_ISR_EXIT_F5E4:
+	CACTL1 &= ~BIT0;
+//	BIC.W   #0x0010,0x0008(SP)
+//	POP.W   R14
+//	POP.W   R15
+//	POP.W   R12
+//	POP.W   R13
 }
 
 //TODO:Unclear feature
 void f_f888(void)
 {
+	uint8_t r14;
+	uint8_t r15;
 
+	*(uint16_t*)0x0210 &= ~BIT6;
+	r14 = *(uint8_t*)0x0224 & 0x0F;
+	*(uint8_t*)0x023e = r14;
+	r15 = 0;
+	if(0==BIT0 & r14)
+	{
+		*(uint8_t*)0x023d = BIT1;
+		r14 = 0xc0;
+	}
+	else
+	{
+		*(uint8_t*)0x023e = 1 + *(uint8_t*)0x023e;
+		*(uint8_t*)0x023d = BIT3;
+		r14 = *(uint8_t*)0x0224;
+	}
+
+//l_f8b8 whole
+
+l_f8c0:
+	TA0CCR0 = 0x998;
+	TA0CTL |= TACLR;
+	TA0CCTL0 &= ~TAIFG;
+	CACTL1 &= ~CAIFG;
+l_f8d2:
+	if(CAIFG & CACTL1)
+	{
+		TA0CTL |= TACLR;
+		TA0CCTL0 &= ~TAIFG;
+		if(r14 >= 0)
+		{
+			TA0CCR0 = 0x0028;
+			while(0 == TA0CCTL0 & TAIFG)
+			{
+				;
+			}
+		}
+		else
+		{
+			TA0CCR0 = 0x0302;
+
+			while(0==(BIT0 & TA0CCTL0))
+			{
+				;
+			}
+
+			TA0CCTL0 &= ~BIT0;
+			TA0CCR0 = 0x042e;
+			MeasCAOut_fcb2();
+			if(0==*(uint16_t*)0x0206)
+			{
+				*(uint16_t*)0x0210 |= BIT6;
+				return;
+			}
+
+			while(0==(BIT0 & TA0CCTL0))
+			{
+				;
+			}
+		}
+	}
+	else
+	{
+		//l_f8f2->l_f8f9
+		if(TA0CCTL0 & BIT0)
+		{
+			*(uint16_t*)0x0210 |= BIT6;
+			return;
+		}
+		else
+		{
+			goto l_f8d2;
+		}
+	}
+
+	P1OUT |= BIT2;
+	TA0CCTL0 &= ~BIT0;
+	TA0CCR0 += 0x01e0;
+	while(0==(BIT0 & TA0CCTL0))
+	{
+		;
+	}
+
+	P1OUT &= ~BIT2;
+	TA0CCTL0 &= ~BIT0;
+	TA0CCR0 = 0x074e;
+	while(0==(BIT0 & TA0CCTL0))
+	{
+		;
+	}
+
+	r14 <<= 1;
+	*(uint8_t*)0x023d = *(uint8_t*)0x023d - 1;
+	if(0 != *(uint8_t*)0x023d)
+	{
+		goto l_f8c0;
+	}
+	if(1 == *(uint8_t*)0x023e)
+	{
+		return;
+	}
+	r15 ++;
+	if(r15 != *(uint8_t*)0x023e)
+	{
+		r14 = *(uint8_t*)(0x0224 + r15);
+		goto l_f8c0;
+	}
+	TA0CTL |= BIT2;
+	TA0CCR0 = 0x07d0;
+	TA0CCTL0 &= ~BIT0;
+	CACTL1 &= ~BIT0;
+
+	do{
+		if(BIT0 & CACTL1)
+		{
+			*(uint8_t*)0x0210 &= ~BIT4;
+			return;
+		}
+	}while(0 == (BIT0 & TA0CCTL0));
+//
+//l_f984:
+//	*(uint16_t*)0x0210 |= BIT6;
+//	return;
 }
 
 //Flash Operation
@@ -73,7 +332,7 @@ void f_f98c(void)
 		p_r14 = 0x1012;
 	}
 
-	WDTCTL = 0x5a80;
+    WDTCTL = WDTPW | WDTHOLD;
 	FCTL2 = 0xa542;
 	FCTL3 = 0xa500;
 	FCTL1 = 0xa502;
@@ -324,7 +583,7 @@ void f_fbcc(void)
 	    	;
 	    }
 	    TA1CCTL0 &= ~TAIFG;
-	    WDTCTL = 0x5a0d;
+	    WDTCTL = WDT_ARST_250;
 	}
 
 	TA1CTL = 0;
@@ -436,7 +695,7 @@ void Clr210_fcde(void)
 /*
  * main.c
  */
-int main(void) {
+void main(void) {
 	// Stop watchdog timer
     WDTCTL = WDTPW | WDTHOLD;
     // Set DCO as 1MHz
@@ -463,9 +722,269 @@ int main(void) {
     __enable_interrupt();
 
     f_fbcc();
+    goto l_f676;
+l_f648:
+	*((uint16_t*)0x0216) = 0;
+	*((uint8_t*)0x0247) = *((uint8_t*)0x0247) + 1;
+l_f650:
+	*((uint16_t*)0x0216) = *((uint16_t*)0x0216) + 1;
+l_f654:
+	if(0x00a0 != *((uint16_t*)0x0218))
+	{
+		if(0==(0x0220 & *((uint16_t*)0x0210)))
+		{
+			if(0x00a0 != *((uint16_t*)0x0246))
+			{
+				// Stop watchdog timer
+			    WDTCTL = WDTPW | WDTHOLD;
+			    __bis_SR_register(CPUOFF);
+			}
+		}
+	}
+l_f676:
+	WDTCTL = WDT_ARST_250;
 
-	while(1) {
+	if(0x00a0 == *((uint16_t*)0x0218))
+	{
+		*((uint16_t*)0x0218) = 0;
+		if(0x64 == *((uint8_t*)0x020c))
+		{
+				if(0==BIT2 & *((uint8_t*)0x021a))
+				{
+					f_fbcc();
+				}
+				*((uint8_t*)0x021a) = 0x3d;
+		}
+		else
+		{
+			if(0x65 == *((uint8_t*)0x020c))
+			{
+				*((uint8_t*)0x021a) = 0x38;
+			}
+			else
+			{
+				if(0x67 == *((uint8_t*)0x020c))
+				{
+					if(0==BIT2 & *((uint8_t*)0x021a))
+					{
+						f_fbcc();
+					}
+					*((uint8_t*)0x021a) = 5;
+				}
+				else
+				{
+					*((uint8_t*)0x021a) = 0;
+				}
+			}
+		}
+	}
+l_f6d0:
+	if(0x00a0 == *((uint8_t*)0x0246))
+	{
+		*((uint8_t*)0x0246) = 0;
+		if(BIT5 & *((uint16_t*)0x0210))
+		{
+			if((0x64 == *((uint8_t*)0x020c)) || (0x65 == *((uint8_t*)0x020c)))
+			{
+				*((uint8_t*)0x0246) = *((uint8_t*)0x021b) | BIT1;
+				goto l_f726;
+			}
+			else
+			{
+				*((uint8_t*)0x0246) = *((uint8_t*)0x021b) | BIT0;
+				goto l_f726;
+			}
+		}
+		else
+		{
+			goto l_f700;
+		}
+	}
+	else
+	{
+		goto l_f7aa;
+	}
+l_f700:
+	if(0 != *((uint8_t*)0x021b) & BIT0)
+	{
+		*((uint8_t*)0x021b) &= ~BIT0;
+		goto l_f726;
 	}
 
-	return 0;
+	if(0 != *((uint8_t*)0x021b) & BIT1)
+	{
+		if((0x64 == *((uint8_t*)0x020c)) || (0x65 == *((uint8_t*)0x020c)))
+		{
+			*((uint8_t*)0x021b) &= ~BIT1;
+		}
+	}
+l_f726:
+	if(0 != *((uint16_t*)0x0210) & BIT9)
+	{
+		*((uint8_t*)0x0246) = *((uint8_t*)0x021b) | BIT1;
+	}
+	else if((0 != *((uint8_t*)0x021b) & BIT1) &&
+		(0x64 != *((uint8_t*)0x020c)) &&
+		(0x65 != *((uint8_t*)0x020c)))
+	{
+		*((uint8_t*)0x021b) &= ~BIT1;
+	}
+
+	if(0 != *((uint8_t*)0x021b))
+	{
+		if(0 == *((uint8_t*)0x0247))
+		{
+			TA1CTL = 0x0214;
+			TA1CCR0 = 0x1388;
+			TA1CCTL0 &= ~BIT0;
+			*((uint16_t*)0x0216) = 0;
+			P2OUT &= BIT1;
+			*((uint8_t*)0x0247) = BIT0;
+		}
+		else
+		{
+			goto l_f776;
+		}
+	}
+	else
+	{
+		goto l_f792;
+	}
+l_f776:
+	*((uint8_t*)0x0245) = 0x28;
+	if(0 != *((uint8_t*)0x021b) & BIT1)
+	{
+		*((uint16_t*)0x0214) = 0x00fa;
+		goto l_f7aa;
+	}
+	else
+	{
+		*((uint16_t*)0x0214) = 0x190;
+		goto l_f7aa;
+	}
+l_f792:
+	TA1CTL = 0;
+	TA1CCTL0 &= ~BIT0;
+	*((uint8_t*)0x0247) = 0;
+	*((uint16_t*)0x0216) = 0;
+	P2OUT &= ~BIT1;
+	P2OUT &= ~BIT2;
+l_f7aa:
+	if(0==(TA1CCTL0 & BIT0))
+	{
+		goto l_f654;
+	}
+	TA1CCTL0 &= ~BIT0;
+	if(*((uint8_t*)0x0247) < 6)
+	{
+		goto l_f7e0;
+	}
+	if(*((uint16_t*)0x0216) >= *((uint8_t*)0x0245))
+	{
+		goto l_f7cc;
+	}
+	P2OUT |= BIT1;
+	goto l_f650;
+l_f7cc:
+	if(*((uint16_t*)0x0216) >= *((uint16_t*)0x0214))
+	{
+		goto l_f7da;
+	}
+	P2OUT &= ~BIT1;
+	goto l_f650;
+l_f7da:
+	*((uint16_t*)0x0216) = 0;
+	goto l_f650;
+l_f7e0:
+	if(*((uint8_t*)0x0247) != 1)
+	{
+		goto l_f800;
+	}
+	if(*((uint16_t*)0x0216) != 0)
+	{
+		goto l_f7f2;
+	}
+	P2OUT &= ~BIT2;
+	goto l_f650;
+l_f7f2:
+	if(*((uint16_t*)0x0216) >= 0x0096)
+	{
+		goto l_f648;
+	}
+	P2OUT &= ~BIT2;
+	goto l_f650;
+l_f800:
+	if(*((uint8_t*)0x0247) != 2)
+	{
+		goto l_f820;
+	}
+	if(*((uint16_t*)0x0216) >= 2)
+	{
+		goto l_f812;
+	}
+	P2OUT |= BIT2;
+	goto l_f650;
+l_f812:
+	if(*((uint16_t*)0x0216) >= 0x0096)
+	{
+		goto l_f648;
+	}
+	P2OUT &= ~BIT2;
+	goto l_f650;
+l_f820:
+	if(*((uint8_t*)0x0247) != 3)
+	{
+		goto l_f842;
+	}
+	if(*((uint16_t*)0x0216) >= 2)
+	{
+		goto l_f834;
+	}
+	P2OUT |= BIT2;
+	goto l_f650;
+l_f834:
+	if(*((uint16_t*)0x0216) >= 0x0096)
+	{
+		goto l_f648;
+	}
+	P2OUT &= ~BIT2;
+	goto l_f650;
+l_f842:
+	if(*((uint8_t*)0x0247) != 4)
+	{
+		goto l_f864;
+	}
+	if(*((uint16_t*)0x0216) >= 0x001e)
+	{
+		goto l_f856;
+	}
+	P2OUT |= BIT2;
+	goto l_f650;
+l_f856:
+	if(*((uint16_t*)0x0216) >= 0x0096)
+	{
+		goto l_f648;
+	}
+	P2OUT &= ~BIT2;
+	goto l_f650;
+l_f864:
+	if(*((uint8_t*)0x0247) != 5)
+	{
+		goto l_f650;
+	}
+	if(*((uint16_t*)0x0216) >= 0x0032)
+	{
+		goto l_f87a;
+	}
+	P2OUT |= BIT2;
+	goto l_f650;
+l_f87a:
+	if(*((uint16_t*)0x0216) >= 0x0096)
+	{
+		goto l_f648;
+	}
+	P2OUT &= ~BIT2;
+	goto l_f650;
+
+	return;
 }
